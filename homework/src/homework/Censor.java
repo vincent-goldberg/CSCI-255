84728cc505fb6c27f40d12ec173ff4f912b2ac49
Vince <vigold@iu.edu>
2025-09-15 03:07:25 +0200

    Created src directory & hw1 skeleton

---
+++ homework/src/homework/Censor.java
@@ -0,0 +1,20 @@
+package homework;
+
+/**
+ * Name: Vincent Goldberg
+ * Date: 16 Sep 2025
+ * Homework #: 1
+ * Source File: Censor.java
+ * Class: C201
+ * Action: The program censors the word "hell" in a sentence using character arrays.
+ */
+import java.util.Scanner;
+
+public class Censor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
